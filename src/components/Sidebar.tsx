b8e5995314892916be927d2faa58abb652789af5
Ivan Seibel <me@ivanseibel.dev>
2022-07-15 17:28:25 +0100

    chore: implement basic structure for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,47 @@
+import { gql, useQuery } from "@apollo/client";
+import { Lesson } from "./Lesson";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string;
+    lessonType: 'live' | 'class';
+    availableAt: Date;
+    title: string;
+    slug: string;
+  }[]
+};
+
 export function Sidebar() {
-    return (
-      <h1>Sidebar</h1>
-    )
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-x-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Lessons schedule
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => (
+          <Lesson 
+            key={lesson.id}
+            availableAt={new Date(lesson.availableAt)} 
+            slug={lesson.slug} 
+            title={lesson.title} 
+            type={lesson.lessonType} 
+          />
+        ))}  
+      </div>
+    </aside>
+  )
   }
